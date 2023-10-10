a8fab0cdfc6d45115c0dd8189a9a0612f83343cf
Kononchuk Anastasiia <notnastycode@gmail.com>
2023-10-10 14:03:38 +0300

    Added Products Scriptable objects & sprites

---
+++ Assets/Scripts/Products/ProductScriptableObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Product", menuName = "Product")]
+public class ProductSO : ScriptableObject
+{
+    public Sprite ProductImage;
+}
